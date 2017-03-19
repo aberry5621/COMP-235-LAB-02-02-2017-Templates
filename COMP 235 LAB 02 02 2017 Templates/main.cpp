f0e3cd1f7ea510afa01d47ec4540560882a94430
gb_man <gb_man@gb-mans-iMac.local>
2017-03-19 14:00:19 -0700

    Initial Commit

---
+++ COMP 235 LAB 02 02 2017 Templates/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP 235 LAB 02 02 2017 Templates
+//
+//  Created by gb_man on 3/19/17.
+//  Copyright © 2017 alex. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
